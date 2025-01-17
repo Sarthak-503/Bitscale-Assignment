f38127ca37ce1208c76f75e1718b05e3d3244c59
unknown <sarthak@enablero.com>
2025-01-17 15:37:05 +0530

    completed Navbar

--- src/components/Common/OperationalBar/OperationPanel.tsx
+++ src/components/Common/OperationalBar/OperationPanel.tsx
@@ -10,22 +10,39 @@ const OperationPanel = () => {
           display: "flex",
           flexDirection: "row",
           width: "16%",
+          height:'inherit',
+          mr:'1'
         }}
       >
         <Button
           variant="outlined"
+          size="small"
           startIcon={<AutoAwesomeOutlinedIcon />}
           sx={{
             background: "#1F2A37",
             color: "white",
             borderRadius: 1,
+            fontSize:'0.7rem',
           }}
         >
           Enrich
         </Button>
-        <Box>
-          <ShareOutlinedIcon />
-          <SaveAltIcon />
+        <Box sx={{
+          display:'flex',
+          flexDirection:'row',
+          justifyContent:'space-evenly',
+          alignItems:'center',
+          ml:2, // 15.168px
+        }}>
+          <ShareOutlinedIcon fontSize="small" sx={{
+            mx:'0.2rem',
+            p:'0.1rem',
+
+          }}/>
+          <SaveAltIcon fontSize="small" sx={{
+            mx:'0.2rem',
+            p:'0.1rem',
+          }}/>
 
           {/* <FontAwesomeIcon icon="fa-solid fa-trash-can" /> */}
         </Box>
